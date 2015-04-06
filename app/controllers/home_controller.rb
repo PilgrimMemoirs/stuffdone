7bff08c877238e6643792566eb5941362f9fc548
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2015-04-06 00:15:18 -0500

    setup devise authentication views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+
+	def index
+		@accomplishments = Accompllishment.all
+	end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,6 @@
 class UserController < ApplicationController
+	protect_from_forgery with: :exception
 	before_action :authenticate_user!
 
-	
+
 end

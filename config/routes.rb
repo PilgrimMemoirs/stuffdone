a3012cb4add7113cd49dcddcb00bb4c69877c362
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2015-04-18 15:16:46 -0500

    added about page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resources :accomplishment
+  resources :accomplishment, :category
+
+  get '/about', to: 'home#about'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

7fea0018bbe902e58eab240147985eac775d4d67
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2015-04-18 15:22:37 -0500

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406021856) do
+ActiveRecord::Schema.define(version: 20150412011124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accomplishment_cats", force: :cascade do |t|
+    t.integer  "accomplishment_id"
+    t.integer  "category_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "accomplishments", force: :cascade do |t|
     t.string   "title"
